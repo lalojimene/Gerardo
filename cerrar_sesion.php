b60b6ee3bbe7ed9d1beaae6ec148a493aa7121d5
lalojimene <gerardojimenezvelasco20@gmail.com>
2025-03-03 15:17:57 -0600

    eliminado

--- cerrar_sesion.php
+++
@@ -1,8 +0,0 @@
-<?php
-// Eliminar la cookie que contiene el token JWT
-setcookie("token", "", time() - 3600, "/"); // Establece la cookie con una fecha de expiración pasada
-
-// Redirigir al usuario a la página de inicio de sesión
-header("Location: login.php");
-exit();
-?>
