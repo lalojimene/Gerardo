ce1de564cef42f441285c3991d2d5185421db343
cristiantru <140206787+cristiantru@users.noreply.github.com>
2025-03-26 11:56:37 -0600

    Eliminar archivos innecesarios

--- README.md
+++
@@ -1,1 +0,0 @@
-en la rama master solo se encuentra nada mas con la autenticacion unica y la rama principal que es  main se encuentra el incio de sesion con token jwt y multifactor
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-setcookie("jwt", "", time() - 3600, "/", "", false, true); // Expira el JWT
-session_destroy();
-header("Location: login.php");
-exit();
-?>
